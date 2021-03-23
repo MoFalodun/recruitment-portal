15f7a1f51f00a443c7deab53dea7baa78ab83d3b
Falodun Modupe <falodunmodupeola@gmail.com>
2021-03-23 13:57:18 +0100

    added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 const { userRouter } = require('./routes');
 
 require('dotenv').config();
@@ -7,6 +8,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 app.use(userRouter);
