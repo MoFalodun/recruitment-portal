333a1db677221d912a2d5c3155a289e6019cabd5
Falodun Modupe <falodunmodupeola@gmail.com>
2021-03-31 17:27:57 +0100

    cloudinary

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,4 +1,4 @@
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require('cloudinary');
 const Datauri = require('datauri');
 const { config } = require('dotenv');
 const { cloudinaryConfig } = require('../services')
